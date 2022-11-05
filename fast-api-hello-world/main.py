b4c8300e0ffe08c34eb3ef44bf39a92603ef279c
fernando <gfesquivelt@unal.edu.co>
2022-11-05 11:55:37 -0500

    Ending fastapi fundamentals

--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -65,12 +65,14 @@ def show_person(
         min_length=1,
         max_length=50,
         title="Person Name",
-        description="This is the person name. It's between 1 and 50 characters"
+        description="This is the person name. It's between 1 and 50 characters",
+        example = "Miguel"
         ),
     age: int = Query(
         ...,
         title="Person Age",
-        description="This is the person age. It's required"
+        description="This is the person age. It's required",
+        example=25
         )
 ):
     return{name: age}
@@ -83,7 +85,8 @@ def show_person(
         ...,
         gt=0,
         title="Person ID",
-        description="This is the person ID. It's greater than 0")
+        description="This is the person ID. It's greater than 0",
+        example=123)
 ):
     return{person_id: "It exists!"}
 
@@ -95,7 +98,8 @@ def update_person(
         ...,
         title="Person ID",
         description="This is the person ID",
-        gt=0
+        gt=0,
+        example=123
     ),
     person: Person = Body(...),
         Location: Location = Body(...)
