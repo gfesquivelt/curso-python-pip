c2e6a4a750caf6e311638aefd5ac812c940a33ba
fernando <gfesquivelt@unal.edu.co>
2022-11-04 21:40:20 -0500

    Path parameter validation

--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 
 #fastapi
 from fastapi import FastAPI
-from fastapi import Body, Query
+from fastapi import Body, Query, Path
 
 app = FastAPI() #declaración del objeto
 
@@ -32,7 +32,29 @@ def create_person(person: Person = Body(...)): #dentro de la función se declara
 
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: int = Query(...)
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title="Person Name",
+        description="This is the person name. It's between 1 and 50 characters"
+        ),
+    age: int = Query(
+        ...,
+        title="Person Age",
+        description="This is the person age. It's required"
+        )
 ):
     return{name: age}
+
+#Validation: path parameters
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Person ID",
+        description="This is the person ID. It's greater than 0")
+):
+    return{person_id: "It exists!"}
