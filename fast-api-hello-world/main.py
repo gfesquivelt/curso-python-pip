a3b48cb4b39523a534a5a9890d636dd1f4d82aa8
fernando <gfesquivelt@unal.edu.co>
2022-11-04 20:36:54 -0500

    Request body creation in fastapi

--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -1,10 +1,35 @@
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#fastapi
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI() #declaración del objeto
+
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None #None es el valor tomado por defecto
+    is_married: Optional[bool] = None
 
 @app.get("/") #path operation decorator
 def home():
     return {"Hello": "World"}
+
+#request and response body
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)): #dentro de la función se declara el request body --> lo que se ve entre parentesis es (parámetro:tipo = Body)
+    return person                              #los ... indican que el request es obligatorio
+
+
+
 
 # def run():
 #     pass
